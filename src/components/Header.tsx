a91c3115534b44c69ee0a01fd4098f1483592a9b
kian <kiankheirani@gmail.com>
2025-03-07 12:30:41 +0330

    'add-header'

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+
+type name = {
+  title: string;
+  subtitle: string;
+};
+export default function Header({ title, subtitle }: name) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#102C57",
+        color: "white",
+        display: "flex",
+        gap: "5px",
+        paddingX: "20px",
+        flexDirection: "column",
+      }}
+    >
+      <Typography
+        sx={{
+          fontWeight: "bold",
+          fontSize: { xs: "25px", lg: "30px" },
+          textTransform: "uppercase",
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography sx={{ fontSize: "15px", textTransform: "capitalize" }}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+}
